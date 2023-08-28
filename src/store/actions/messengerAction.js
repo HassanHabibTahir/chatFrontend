07c1ae64196c0a1c15a8e028361dbab1c5a10de3
Hassan Habib Tahir <47211929+HassanHabibTahir@users.noreply.github.com>
2023-08-28 18:35:50 +0500

    added by hassan habib tahir

---
+++ src/store/actions/messengerAction.js
@@ -0,0 +1,84 @@
+import axios from 'axios';
+import { FRIEND_GET_SUCCESS, MESSAGE_GET_SUCCESS, MESSAGE_SEND_SUCCESS, THEME_GET_SUCCESS, THEME_SET_SUCCESS } from "../types/messengerType";
+import { url } from '../../config';
+
+
+export const getToken = async () => {
+    const getToken = await localStorage.getItem('authToken');
+    const token = JSON.parse(getToken);
+    return token.token
+
+    //  return getToken
+}
+
+
+
+export const getFriends = (id) => async (dispatch) => {
+    try {
+        let token = await getToken();
+        const response = await axios.get(`${url}/api/user/allUser`, {
+            headers: {
+                'authorization': token
+            }
+        })
+        if (response) {
+
+            dispatch({
+                type: FRIEND_GET_SUCCESS,
+                payload: {
+                    friends: response?.data
+                }
+            })
+
+        }
+
+    } catch (error) {
+        console.log(error.response.data);
+    }
+}
+
+export const messageSend = (data) => async (dispatch) => {
+    try {
+        console.log(data, "data--->");
+        let token = await getToken();
+        const response = await axios.post(`${url}/api/message`, data, {
+            headers: {
+                'authorization': token
+            }
+        });
+        dispatch({
+            type: MESSAGE_SEND_SUCCESS,
+            payload: {
+                message: response.data
+            }
+        })
+    } catch (error) {
+        console.log(error.response.data);
+    }
+}
+
+export const getMessage = (id) => {
+
+    return async (dispatch) => {
+        try {
+            let token = await getToken();
+            const response = await axios.get(`${url}/api/message/${id}`, {
+
+                headers: {
+                    'authorization': token
+                }
+            });
+
+            dispatch({
+                type: MESSAGE_GET_SUCCESS,
+                payload: {
+                    message_get_success: true,
+                    message: response?.data?.data
+                }
+            })
+        } catch (error) {
+            console.log(error.response.data)
+        }
+    }
+}
+
